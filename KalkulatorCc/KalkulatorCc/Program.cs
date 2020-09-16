24e5b6bb94d8d7ce030da186bf622b4d31a03ce2
Muhamad ZIbran Fitadiyatama <70439278+ZibranovSky@users.noreply.github.com>
2020-09-16 08:10:00 +0700

    Add files via upload

---
+++ KalkulatorCc/KalkulatorCc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace KalkulatorCc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan angka pertama !");
+            int angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua !");
+            int angka2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan Simbol Hitung!");
+            string simbol = Console.ReadLine();
+
+            switch (simbol)
+            {
+                case("+"):
+                    Console.WriteLine(angka1 + angka2);
+                    break;
+                case ("-"):
+                    Console.WriteLine(angka1 - angka2);
+                    break;
+                case ("/"):
+                    Console.WriteLine(angka1 / angka2);
+                    break;
+                case ("*"):
+                    Console.WriteLine(angka1 * angka2);
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+    }
+}
